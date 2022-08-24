c105037dd2246b9be5d033be1e05deb07d697c5a
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-24 15:13:35 +0000

    [Finish] アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one_attached :image
+  has_many :books, dependent: :destroy
          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
   get 'homes/about' => 'homes#about', as: 'about'
   
  resources :users, only: [:show]
