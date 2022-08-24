0ef692bc358382b1a0fa4172bbf5a46409dcfa17
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-24 19:47:28 +0000

    [Create] user/showページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+  
 end
