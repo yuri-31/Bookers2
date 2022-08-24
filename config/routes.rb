e51f9a3a53cafe6b216cfa146cc985c850d4474f
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-24 17:31:38 +0000

    [Create] new/create機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   
  resources :users, only: [:show]
- resources :books, only: [:new, :show, :index]
+ resources :books, only: [:new, :create, :show, :index, :destroy, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
