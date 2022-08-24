63194a9cfe88d3eccf904ce15c1a05d0260ee4dd
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-24 18:54:46 +0000

    [Create] showページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   get 'homes/about' => 'homes#about', as: 'about'
   
- resources :users, only: [:show]
+ resources :users, only: [:show, :edit, :update]
  resources :books, only: [:new, :create, :show, :index, :destroy, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
