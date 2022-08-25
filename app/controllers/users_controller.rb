eab5b80d738aac489c2da13406aec3f1090e49e3
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-25 18:44:28 +0000

    [Create] user-edit/update機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,18 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    user = User.find(params[:id])
+    user.save(user_params)
+    redirect_to books_path
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
 end
