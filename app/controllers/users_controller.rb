74976aa54f8278a8fe53a640431e37989746d16f
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-26 20:43:57 +0000

    [Create] user/index機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def index
+    @users = User.all
+  end
+  
   def edit
     @user = User.find(params[:id])
   end
